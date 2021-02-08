f5fe6b3749f7f8ba343ba52abcfbf8c00ea5c991
big_richi <big.richi@outlook.com>
2021-02-08 09:57:23 -0500

    commit tree 8b8cf441fc6361d2e17806155f17114ee9566fac

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :name, :hash, :student, :grade
+
+    def initialize(name)
+        @name = name
+        @hash = Hash.new{}
+
+    end
+
+    def roster
+        hash 
+    end
+
+    def add_student(student, grade)
+        # binding.pry
+        if hash[grade]
+            hash[grade] << student
+        else
+            hash[grade] = []
+            hash[grade] << student
+        end
+        hash
+    end
+
+    def grade(grade)
+        hash[grade]
+    end
+
+    def sort
+        hash.sort.to_h
+    end
+end
+
+# school = School.new("Bayside High School")
+# p school.roster
+# school.add_student("Zach Morris", 9)
+# school.add_student("Nick Morris", 9)
+# school.add_student("Jamie Morris", 9)
+# school.add_student("Jamie Morris", 8)
+# p school.roster
+# p school.sort
+
+# school.roster
